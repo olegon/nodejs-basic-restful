dffb8f23b616f59b1da5c26a08504d01576943c5
olegon <olegon.main@gmail.com>
2017-09-05 15:38:13 -0300

    Adicionado log na aplicação (development)

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const db = require('./db');
 const userController = require('./user/user-controller');
 const authController = require('./auth/auth-controller');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+else {
+    app.use(morgan('common'));
+}
 
 app.use('/users', userController);
 app.use('/auth', authController);
